1e725fc399f71678a4b3203dcd6bc578a7d1b3cd
sevtechcodes <sevimtas0101@gmail.com>
2024-06-14 14:33:24 +0200

    first 5 chapters are done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+  {/* This is called a root layout and is required. Any UI you add to the root layout will be shared across all pages in your application.*/} 
+
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +10,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/* <body>{children}</body> */} 
+			{/* By adding Inter to the <body> element, the font will be applied throughout your application. Here, you're also adding the Tailwind antialiased class which smooths out the font.  */}
+			<body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+// Let's add a custom Google font to your application to see how this works!
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({  
+  weight: ['400', '700'],
+  subsets: ['latin'],
+
+});
